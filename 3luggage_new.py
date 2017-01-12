37eef14ac47803cd4533fd2d0f1908ce3bea52a1
Charlie <memsding@gmail.com>
2017-01-12 15:14:43 -0800

    delete redundancy

--- 3luggage_new.py
+++ 3luggage_new.py
@@ -16,24 +16,6 @@ def luggage1(weights):
     container_list.reverse()
     result = [x for c in container_list for x in c]
     return ' '.join([str(x) for x in result])
-    # 40,39,5,6,2,1,1,1,30
-
-    # for i, lug in enumerate(lug_list):
-    #     count += lug
-    #     # lug_list.remove(lug)
-    #     if count < containerWeight:
-    #         container.append(lug)
-    #     if count >= containerWeight:
-    #         container_list.append(container)
-    #         count = 0
-    #         container = []
-    #         container.append(lug)
-    #         # print container_list,container
-    #     if i == len(lug_list)-1 and container not in container_list:
-    #         container_list.append(container)
-    # container_list.reverse()
-    # result = [x for container in container_list for x in container]
-    # return ','.join([str(x) for x in result])
 
 if __name__ == '__main__':
     print luggage1('2,1,1,1,30,5,6,39,40')
